c4ac42c1fea42edd5d4280affc8a8c0ec03f2687
Jorge Arboleda <jarboleda@justcapital.media>
2024-06-06 08:32:44 -0500

    taller 10 ya funciona

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { RecursosService } from './servicios/recursos.service';
 import { ContactoComponent } from './shared/contacto/contacto.component';
 import { DataTableComponent } from './shared/tabla/data-table.component';
 import { HttpClientModule } from '@angular/common/http';
 import { Foto } from './interfaz/foto';
 import { UserTableComponent } from './shared/user-table/user-table.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactoComponent, DataTableComponent, UserTableComponent, HttpClientModule],
+  imports: [RouterOutlet, ContactoComponent, DataTableComponent, UserTableComponent, HttpClientModule, AboutComponent, RouterLink, HomeComponent],
   providers: [RecursosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
